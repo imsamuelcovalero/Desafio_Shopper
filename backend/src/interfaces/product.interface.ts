f823a56688d7a375f77e0a4e20235c1b1153bb60
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2023-05-15 13:13:12 -0300

    improvement(validateNewData): Retira a lógica de validação de newPrice ser um valor numérico válido do Joi e leva para a função principal para poder retornar a mensagem ao frontend em caso de erro.

--- backend/src/interfaces/product.interface.ts
+++ backend/src/interfaces/product.interface.ts
@@ -8,11 +8,6 @@ export interface IProductUpdate {
   newPrice: number;
 }
 
-// export interface IProductUpdate {
-//   codigo: string;
-//   novoPreco: number;
-// }
-
 export interface ProductsController {
   validateNewData(req: IBody): Promise<void>;
   updateProducts(req: IProductUpdate): Promise<void>;
--- backend/src/services/products.service.ts
+++ backend/src/services/products.service.ts
@@ -1,4 +1,4 @@
-import { IBody, IProductUpdate, IProduct, IProductData } from '../interfaces/product.interface';
+import { IBody, IProductUpdate, IProductData } from '../interfaces/product.interface';
 import ProductModel from '../database/models/product.model';
 import PackModel from '../database/models/pack.model';
 import CustomError from '../errors/CustomError';
@@ -10,7 +10,17 @@ export default class ProductService {
     this.model = new ProductModel();
   }
 
-  private checkPriceAdjustment(parsedNewPrice: number, salesPrice: number, code: number, productData: IProductData): void {
+  // função que valida newPrice para verificar se é um valor numérico valido
+  private validateNewPrice(newPrice: number, productData: IProductData): number {
+    const parsedNewPrice = parseFloat(String(newPrice));
+    if (isNaN(parsedNewPrice)) {
+      productData.status.push('O novo preço deve ser um número válido');
+    }
+    productData.novoPreco = parsedNewPrice;
+    return parsedNewPrice;
+  }
+
+  private checkPriceAdjustment(parsedNewPrice: number, salesPrice: number, productData: IProductData): void {
     const priceDifference = parsedNewPrice - salesPrice;
 
     if (priceDifference < -salesPrice * 0.1 || priceDifference > salesPrice * 0.1) {
@@ -18,15 +28,9 @@ export default class ProductService {
     }
   }
 
-  private checkSalesPrice(parsedNewPrice: number, costPrice: number, code: number, productData: IProductData): void {
+  private checkSalesPrice(parsedNewPrice: number, costPrice: number, productData: IProductData): void {
     if (parsedNewPrice < costPrice) {
       productData.status.push('Preço de venda abaixo do custo para o produto');
-    }
-  }
-
-  private checkProductExists(existingProduct: IProduct | null, code: number, productData: IProductData): void {
-    if (!existingProduct) {
-      productData.status.push('Produto com código ${code} não encontrado');
     }
   }
 
@@ -49,6 +53,40 @@ export default class ProductService {
     }
   }
 
+  private async checkComponentRules(code: number, products: IProductUpdate[], productData: IProductData): Promise<void> {
+    const includedInPacks = await PackModel.findAll({ where: { productId: code } });
+
+    for (const pack of includedInPacks) {
+      const newPricePack = products.find(prod => prod.code === pack.packId);
+      if (!newPricePack) {
+        productData.status.push(`Pacote com código ${pack.packId} que inclui o produto ${code} não está presente na lista de novos preços`);
+        continue;
+      }
+
+      const packItems = await PackModel.findAll({ where: { packId: pack.packId } });
+
+      let sumOfItemPrices = 0;
+      for (const item of packItems) {
+        const newPriceProduct = products.find(prod => prod.code === item.productId);
+
+        if (newPriceProduct) {
+          sumOfItemPrices += newPriceProduct.newPrice * item.qty;
+        } else {
+          const productDetails = await ProductModel.findOne({ where: { code: item.productId } });
+          if (productDetails) {
+            sumOfItemPrices += productDetails.salesPrice * item.qty;
+          } else {
+            productData.status.push(`Produto com código ${item.productId} não encontrado`);
+          }
+        }
+      }
+
+      if (newPricePack.newPrice !== sumOfItemPrices) {
+        productData.status.push(`O preço do pacote de código ${pack.packId} não é igual à soma dos preços dos itens que o compõem`);
+      }
+    }
+  }
+
   validateNewData = async (body: IBody): Promise<Array<IProductData>> => {
     const { products } = body;
 
@@ -60,13 +98,14 @@ export default class ProductService {
       // Verificar se o código do produto existe
       const existingProduct = await ProductModel.findOne({ where: { code } });
 
+      // Se não existir, adicionar o produto com status de erro e ir para o próximo
       if (!existingProduct) {
         const productData: IProductData = {
           codigo: code,
           nome: 'Desconhecido',
           precoAtual: 0,
           novoPreco: newPrice,
-          status: ['Produto não encontrado'],
+          status: ['Produto com código ${code} não encontrado'],
         } as IProductData;
 
         validatedData.push(productData);
@@ -75,81 +114,30 @@ export default class ProductService {
 
       const { salesPrice, costPrice } = existingProduct;
 
-      const parsedNewPrice = parseFloat(String(newPrice));
-
       const productData: IProductData = {
         codigo: code,
         nome: existingProduct.name,
         precoAtual: salesPrice,
-        novoPreco: parsedNewPrice,
+        novoPreco: newPrice,
         status: [],
       } as IProductData;
 
-      // Verificar se o novo preço respeita as regras de negócio
-      this.checkProductExists(existingProduct, code, productData);
-      this.checkPriceAdjustment(parsedNewPrice, salesPrice, code, productData);
-      this.checkSalesPrice(parsedNewPrice, costPrice, code, productData);
+      const parsedNewPrice = this.validateNewPrice(newPrice, productData);
 
-      // Aqui deve ser verificada a regra de negócios para pacotes
+      // Verifica se o novo preço respeita as regras de negócio
+      this.checkPriceAdjustment(parsedNewPrice, salesPrice, productData);
+      this.checkSalesPrice(parsedNewPrice, costPrice, productData);
+
+      // Verifica se o a regra de negócio para pacotes foi respeitada
       if (code.toString().length === 4) {
-        // Procura na PackModel quais são os itens que compõem o pacote e as quantidades
-        const packItems = await PackModel.findAll({ where: { packId: code } });
-
-        let sumOfItemPrices = 0;
-        for (const item of packItems) {
-          // Verifica se o produto está na lista de produtos com novos preços
-          const newPriceProduct = products.find(prod => prod.code === item.productId);
-          if (!newPriceProduct) {
-            throw new CustomError(400, `Produto com código ${item.productId} que faz parte do pacote ${code} não está presente na lista de novos preços`);
-          }
-
-          sumOfItemPrices += newPriceProduct.newPrice * item.qty;
-        }
-
-        if (parsedNewPrice !== sumOfItemPrices) {
-          throw new CustomError(400, `O preço do pacote de código ${code} não é igual à soma dos preços dos itens que o compõem`);
-        }
-        // Aqui deve ser verificada a regra de negócios para componentes
+        await this.checkPackageRules(code, products, parsedNewPrice, productData);
+        // Verifica se o a regra de negócio para componentes foi respeitada
       } else {
-        // Procurar todos os pacotes que incluem este produto
-        const includedInPacks = await PackModel.findAll({ where: { productId: code } });
+        await this.checkComponentRules(code, products, productData);
+      }
 
-        for (const pack of includedInPacks) {
-          // Verificar se o pacote está na lista de produtos com novos preços
-          const newPricePack = products.find(prod => prod.code === pack.packId);
-          if (!newPricePack) {
-            throw new CustomError(400, `Pacote com código ${pack.packId} que inclui o produto ${code} não está presente na lista de novos preços`);
-          }
-
-          // Procurar todos os produtos que fazem parte deste pacote
-          const packItems = await PackModel.findAll({ where: { packId: pack.packId } });
-
-          let sumOfItemPrices = 0;
-          for (const item of packItems) {
-            // Verificar se o produto está na lista de produtos com novos preços
-            const newPriceProduct = products.find(prod => prod.code === item.productId);
-
-            if (newPriceProduct) {
-              // Se o novo preço do produto estiver presente, use-o, caso contrário, use o preço antigo
-              sumOfItemPrices += newPriceProduct.newPrice * item.qty
-            } else {
-              const productDetails = await ProductModel.findOne({ where: { code: item.productId } });
-              if (productDetails) {
-                sumOfItemPrices += productDetails.salesPrice * item.qty;
-              } else {
-                throw new CustomError(400, `Produto com código ${item.productId} não encontrado`);
-              }
-            }
-          }
-
-          if (newPricePack.newPrice !== sumOfItemPrices) {
-            throw new CustomError(400, `O preço do pacote de código ${pack.packId} não é igual à soma dos preços dos itens que o compõem`);
-          }
-        }
-
-        if (productData.status.length === 0) {
-          productData.status.push('OK');
-        }
+      if (productData.status.length === 0) {
+        productData.status.push('OK');
       }
 
       validatedData.push(productData);
