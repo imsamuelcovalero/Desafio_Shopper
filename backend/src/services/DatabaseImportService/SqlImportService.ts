6276bd480b40064d6763209cec7b482c32b7f283
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2023-05-20 14:22:35 -0300

    feat(WriteJSONFileService): Finaliza a lógica da função auxiliar que transcreve as ifnormações do arquivo .sql em um arquivo .json

--- backend/src/services/DatabaseImportService/SqlImportService.ts
+++ backend/src/services/DatabaseImportService/SqlImportService.ts
@@ -2,7 +2,8 @@
 import * as mysql from 'mysql2/promise';
 import * as dbConfig from '../../database/config/database'
 import { readFileSync, writeFileSync } from 'fs';
-import path = require('path');
+// import path = require('path');
+import writeJSONFile from './WriteJSONFileService';
 
 
 const config = dbConfig as {
@@ -34,6 +35,10 @@ class SqlImportService {
   async importSqlFile(filePath: string): Promise<void> {
     let sqlContent = readFileSync(filePath).toString();
 
+    /* Para uso posterior na função de criação do arquivo json, é preciso jogar numa variável a estrutura das tabelas, que está associado ao comando CREATE TABLE. */
+    const tableStructure = sqlContent.match(/CREATE TABLE .*?\n\)/gs);
+    console.log('tableStructureOnSqlImport', tableStructure);
+
     /* Remover instruções "CREATE TABLE". Neste caso não queremos criar as tabelas, pois elas já existem. caso queria executar todo o arquivo, basta remover esta linha. E caso o arquivo possua "DROP TABLE IF EXISTS", também é necessário removê-lo ou deixar ambos, para que após dropar as tabelas possa recriá-las. */
     sqlContent = sqlContent.replace(/CREATE TABLE .*?;/gs, '');
 
@@ -42,16 +47,20 @@ class SqlImportService {
 
     // Dividir o conteúdo em várias queries
     const queries = sqlContent.split(';').map(query => query.trim()).filter(query => query);
-    console.log('queries', queries);
+    // console.log('queries', queries);
 
 
     // Executar cada query
-    for (const query of queries) {
-      await this.executeRawQuery(query);
-    }
+    // for (const query of queries) {
+    //   await this.executeRawQuery(query);
+    // }
 
-    // Após a importação do arquivo SQL e execução das consultas, escreva os dados em um arquivo JSON
-    this.writeToJSONFile(queries);
+    // Chama a função auxiliar para criar o arquivo json
+    if (tableStructure !== null) {
+      writeJSONFile(queries, tableStructure);
+    } else {
+      console.log('Nenhuma estrutura de tabela encontrada');
+    }
   }
 
   async executeRawQuery(query: string): Promise<void> {
@@ -59,12 +68,6 @@ class SqlImportService {
     const connection = await this.pool.getConnection();
     await connection.query(query);
     connection.release();
-  }
-
-  writeToJSONFile(data: any): void {
-    const outputPath = path.join(__dirname, '../../database/database.json');
-    writeFileSync(outputPath, JSON.stringify(data, null, 2));
-    console.log('Os dados foram escritos com sucesso no arquivo database.json');
   }
 }
 
---
+++ backend/src/services/DatabaseImportService/WriteJSONFileService.ts
@@ -0,0 +1,73 @@
+import { writeFileSync } from 'fs';
+import path = require('path');
+
+export default function writeJSONFile(data: string[], tableStructure: string[]): void {
+  const filteredData = data.filter(line => line.startsWith('INSERT INTO'));
+
+  let tableData: any = {};
+  let tableNames: string[] = [];
+
+  /* Para cada linha de estrutura, extrair o nome da tabela e as colunas */
+  tableStructure.forEach((tableStr: string) => {
+    const tableNameMatch = tableStr.match(/(?<=CREATE TABLE ).+?(?=\s)/g);
+    if (!tableNameMatch) {
+      console.error(`Erro ao extrair o nome da tabela de: ${tableStr}`);
+      return; // pula para a próxima iteração do forEach
+    }
+    const tableName = tableNameMatch[0];
+
+    const columnPartMatch = tableStr.match(/(?<=\().+(?=\))/gs);
+    if (!columnPartMatch) {
+      console.error(`Erro ao extrair as partes da coluna de: ${tableStr}`);
+      return; // pula para a próxima iteração do forEach
+    }
+
+    const columnPart = columnPartMatch[0];
+    const columnLines = columnPart.split('\n').map(line => line.trim()).filter(line => line && !line.startsWith('CONSTRAINT') && !line.includes('AUTO_INCREMENT'));
+
+    const columnNames = columnLines.map(line => line.split(' ')[0]); // A primeira palavra de cada linha deve ser o nome da coluna
+
+    if (tableName) {
+      tableData[tableName] = columnNames;
+      tableNames.push(tableName);
+    }
+  });
+
+  /* Para cada linha de dados, extrair o nome da tabela, os valores e criar um objeto com os valores */
+  filteredData.forEach((line: string) => {
+    const tableNameMatch = line.match(/(?<=INSERT INTO ).+?(?=\s)/g);
+    if (!tableNameMatch) {
+      console.error(`Erro ao extrair o nome da tabela de: ${line}`);
+      return; // pula para a próxima iteração do forEach
+    }
+    const tableName = tableNameMatch[0];
+
+    const valuesMatch = line.match(/(?<=VALUES \().+?(?=\))/g);
+    if (!valuesMatch) {
+      console.error(`Erro ao extrair os valores de: ${line}`);
+      return; // pula para a próxima iteração do forEach
+    }
+
+    const tableValues = valuesMatch[0].split(',').map(value => value.trim().replace(/^'(.*)'$/, '$1'));
+    console.log('tableValues', tableValues);
+
+    const newObj = {} as any;
+
+    const columnNames = tableData[tableName];
+    if (columnNames && columnNames.length) {
+      columnNames.forEach((column: string | number, index: string | number) => {
+        newObj[column] = tableValues[+index];
+      });
+    }
+
+    if (!tableData[tableName]) {
+      tableData[tableName] = [];
+    }
+    tableData[tableName].push(newObj);
+  });
+
+  // Escreve o objeto tableData para um arquivo .json
+  const outputPath = path.join(__dirname, '../../database/database.json');
+  writeFileSync(outputPath, JSON.stringify(tableData, null, 2));
+  console.log('Os dados foram escritos com sucesso no arquivo database.json');
+}
